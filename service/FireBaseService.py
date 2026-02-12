79b351816b0da87f19bdfe03040ac94ac3672b28
ThuongDT <thuongdt@ezg.vn>
2026-02-12 15:29:09 +0700

    remove log

--- service/FireBaseService.py
+++ service/FireBaseService.py
@@ -30,7 +30,7 @@ def read_gift_codes():
         print(f'Error reading gift codes: {e}')
         return {}
 
-def create_gift_code(deviceId, packs, rewards):
+def create_gift_code(deviceId, packs, rewards, type = 1):
     doc = DOC_REF.get()
 
     list_code = read_gift_codes()
@@ -45,40 +45,40 @@ def create_gift_code(deviceId, packs, rewards):
         render_code = strService.gen_random_string()
     
     return {
-        render_code: get_gift_code_data(render_code, deviceId, packs, rewards)
+        render_code: get_gift_code_data(render_code, deviceId, packs, rewards, type = type)
     }
 
 def update_gift_code(giftCodes):
-    strService.print_log(giftCodes, "call_update")
+    # strService.print_log(giftCodes, "call_update")
 
-    is_update = False
+    # is_update = False
 
     for gift_code in giftCodes:
         for code, data in gift_code.items():
             DOC_REF.update({
                 f"giftCodes.{code}": data
             })
-            is_update = True
+            # is_update = True
 
-    if is_update:
-        strService.print_log(giftCodes, "update")
+    # if is_update:
+        # strService.print_log(giftCodes, "update")
 
 def delete_gift_code(gift_codes_to_delete):
-    strService.print_log(gift_codes_to_delete, "call_delete")
+    # strService.print_log(gift_codes_to_delete, "call_delete")
 
     if not gift_codes_to_delete:
         return
 
     delete = {f"giftCodes.{code}": DELETE_FIELD for code in gift_codes_to_delete}
     DOC_REF.update(delete)
-    strService.print_log(gift_codes_to_delete, "delete")
+    # strService.print_log(gift_codes_to_delete, "delete")
     print("Deleted!")
 
-def get_gift_code_data(code, deviceId, packs, rewards):
+def get_gift_code_data(code, deviceId, packs, rewards, type = 1):
     gift_code_data = {
         "code": code,
         "deviceId": deviceId,
-        "giftCodeType": 1,
+        "giftCodeType": type,
         "packs": packs,
         "rewards": rewards
     }
